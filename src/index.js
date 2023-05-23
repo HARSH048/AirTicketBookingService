a70d41beaa0c91efc11cb5fe86e90b153abea2e2
harsh <bhardwaj@gmail.com>
2023-05-23 10:22:25 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const app = express();
+const apiRoutes = require("./routes/index");
+// const { PORT } = require("./config/serverconifg");
+const PORT = 3000;
+
+const db = require("./models/index");
+const SetupandStartServer = async () => {
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+  app.listen(PORT, () => {
+    console.log(`server started at ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+  });
+};
+
+SetupandStartServer();
